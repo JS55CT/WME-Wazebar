c39d2625d71d080e13a546d93769be3b5c8b77a5
justins83 <justins83.waze@gmail.com>
2025-04-07 16:03:52 -0400

    Update Wazebar.js

--- Wazebar.js
+++ Wazebar.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Wazebar
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2025.04.06.00
+// @version      2025.04.06.01
 // @description  Displays a bar at the top of the editor that displays inbox, forum & wiki links
 // @author       JustinS83
 // @include      https://beta.waze.com/*
